bfd705d4fde405a96b50e3b26106fc7d5a39cad7
ReFreezed <ReFreezed@users.noreply.github.com>
2022-09-24 20:33:55 +0200

    Can change light reach. Fixed incorrect shadow width. Polish.

--- main.lua
+++ main.lua
@@ -3,8 +3,19 @@
 -- by Marcus 'ReFreezed' Thunström
 -- License: CC0 (https://creativecommons.org/publicdomain/zero/1.0/)
 --
-local LG  = love.graphics
+
+local RENDER_SCALE      = 2  -- Pixel art, yo.
+local WORLD_SCALE_Y     = .7 -- Make the y axis a bit more compact.
+local LIGHT_TRANSITION  = 20 -- Transition length between in-front-of to behind the light.
+local MOVE_SPEED        = 65
+local MODIFICATON_SPEED = 3
+
 local TAU = 2*math.pi
+
+
+
+-- Load graphics.
+local LG = love.graphics
 
 local images = {
 	sprites = LG.newImage("gfx/sprites.png"),
@@ -16,15 +27,12 @@ local quads = {
 	player = LG.newQuad(00,00, 32,32, images.sprites),
 	tree   = LG.newQuad(32,00, 32,32, images.sprites),
 	sign   = LG.newQuad(00,32, 32,32, images.sprites),
+	house  = LG.newQuad(32,32, 32,32, images.sprites),
 }
 
-local RENDER_SCALE     = 2
-local WORLD_SCALE_Y    = .7
-local MOVE_SPEED       = 65
-local LIGHT_MOVE_SPEED = 3
 
 
-
+-- Add entities.
 local worldWidth  = LG.getWidth () / RENDER_SCALE
 local worldHeight = LG.getHeight() / RENDER_SCALE
 local entities    = {}
@@ -38,14 +46,17 @@ end
 local player = addNewEntity("player", quads.player, .5*worldWidth,.5*worldHeight, false)
 
 local rand = love.math.random
-for i = 1, 30 do  addNewEntity("tree", quads.tree, rand(worldWidth),rand(worldHeight), rand(2)==1)  end
-for i = 1, 4  do  addNewEntity("sign", quads.sign, rand(worldWidth),rand(worldHeight), rand(2)==1)  end
+for i = 1, 30 do  addNewEntity("tree" , quads.tree , rand(worldWidth),rand(worldHeight), rand(2)==1)  end
+for i = 1, 4  do  addNewEntity("sign" , quads.sign , rand(worldWidth),rand(worldHeight), rand(2)==1)  end
+for i = 1, 2  do  addNewEntity("house", quads.house, rand(worldWidth),rand(worldHeight), rand(2)==1)  end
 
 local lightSource = addNewEntity("light", nil, 0,0, false)
 lightSource.z     = 50
+lightSource.reach = 300
 
 
 
+-- Prepare perspective correct quad rendering.
 local vertexFormat = {
 	{"VertexPosition", "float", 3}, -- x,y,z
 	{"VertexTexCoord", "float", 2}, -- u,v
@@ -67,7 +78,7 @@ local quadShader = LG.newShader[[//GLSL
 	#ifdef VERTEX
 		vec4 position(mat4 proj, vec4 vertPos) {
 			z         = vertPos.z;
-			vertPos.z = 0.0; // The Z is for the texture, not the vertex.
+			vertPos.z = 0.0; // The Z is only for the pixel shader.
 			return proj * vertPos;
 		}
 	#endif
@@ -94,6 +105,17 @@ local quadShader = LG.newShader[[//GLSL
 
 
 
+--
+-- LÖVE event callbacks.
+--
+
+local function clamp(v, min, max)
+	return math.max(math.min(v, max), min)
+end
+local function clamp01(v)
+	return clamp(v, 0, 1)
+end
+
 function love.update(dt)
 	local moveX = 0
 	local moveY = 0
@@ -110,8 +132,10 @@ function love.update(dt)
 	lightSource.x = love.window.hasMouseFocus() and love.mouse.getX()/RENDER_SCALE or player.x
 	lightSource.y = love.window.hasMouseFocus() and love.mouse.getY()/RENDER_SCALE or player.y
 
-	if love.keyboard.isDown"pageup"   then  lightSource.z = math.max(lightSource.z*(1*LIGHT_MOVE_SPEED)^dt, 32)  end
-	if love.keyboard.isDown"pagedown" then  lightSource.z = math.max(lightSource.z*(1/LIGHT_MOVE_SPEED)^dt, 32)  end
+	if love.keyboard.isDown"1" then  lightSource.z     = clamp(lightSource.z    *(1*MODIFICATON_SPEED)^dt, 4 , 500 )  end
+	if love.keyboard.isDown"2" then  lightSource.z     = clamp(lightSource.z    *(1/MODIFICATON_SPEED)^dt, 4 , 500 )  end
+	if love.keyboard.isDown"3" then  lightSource.reach = clamp(lightSource.reach*(1*MODIFICATON_SPEED)^dt, 20, 1000)  end
+	if love.keyboard.isDown"4" then  lightSource.reach = clamp(lightSource.reach*(1/MODIFICATON_SPEED)^dt, 20, 1000)  end
 end
 
 
@@ -148,9 +172,9 @@ local function sendVec4(shader, var, x,y,z,w)
 	pcall(shader.send, shader, var, vec4)
 end
 
-local function drawPerspectiveCorrectQuad(image, x1,y1,x2,y2,x3,y3,x4,y4, u,v,uvW,uvH)
+local function drawPerspectiveCorrectQuad(image, x1,y1,x2,y2,x3,y3,x4,y4, uvX,uvY,uvW,uvH)
 	-- Calculate fake z-values to fix the perspective.
-	local z1 = 1
+	local z1 = 1 -- Reference corner - is always 1.
 	local z2 = 1
 	local z3 = 1
 	local z4 = 1
@@ -184,7 +208,7 @@ local function drawPerspectiveCorrectQuad(image, x1,y1,x2,y2,x3,y3,x4,y4, u,v,uv
 	-- Draw!
 	quadMesh:setTexture(image)
 	quadMesh:setVertices(vertices)
-	sendVec4(quadShader, "quad", u,v,uvW,uvH)
+	sendVec4(quadShader, "quad", uvX,uvY,uvW,uvH)
 
 	LG.setShader(quadShader)
 	LG.draw(quadMesh)
@@ -208,10 +232,6 @@ end
 
 
 
-local function clamp(v, min, max)
-	return math.max(math.min(v, max), min)
-end
-
 function love.draw()
 	table.sort(entities, function(a, b)
 		if a.y ~= b.y then  return a.y < b.y  end
@@ -224,17 +244,25 @@ function love.draw()
 
 	-- Light (on ground).
 	local w,h = images.light:getDimensions()
-	LG.setColor(.3, .6, .3) ; LG.draw(images.light, lightSource.x,lightSource.y, 0, 25,25*WORLD_SCALE_Y, w/2,h/2)
-	LG.setColor(1, 1, 1)    ; LG.draw(images.light, lightSource.x,lightSource.y, 0, 01,01*WORLD_SCALE_Y, w/2,h/2)
+	local sx  = .05 * lightSource.reach
+	local sy  = .05 * lightSource.reach * WORLD_SCALE_Y
+	LG.setColor(.3, .6, .3) ; LG.draw(images.light, lightSource.x,lightSource.y, 0, sx,sy,           w/2,h/2)
+	LG.setColor(1, 1, 1)    ; LG.draw(images.light, lightSource.x,lightSource.y, 0, 1,WORLD_SCALE_Y, w/2,h/2)
 
 	-- Entity shadows.
 	for _, e in ipairs(entities) do
-		if e.type == "light" then
-			-- void
-
-		else
+		if e.type ~= "light" then
 			local iw,ih      = images.sprites:getDimensions()
 			local qx,qy, w,h = e.quad:getViewport()
+			local xL         = e.x - w/2
+			local xR         = e.x + w/2
+			local y          = e.y
+
+			-- If the light is too low then we can't render the top part of the
+			-- shadow as it would be above the horizon, so we just cut it off.
+			local shadowToCutOff = math.max(h-lightSource.z, 0)
+			qy = qy + shadowToCutOff
+			h  = h  - shadowToCutOff
 
 			--
 			--          x light
@@ -249,7 +277,8 @@ function love.draw()
 			--  tanθ     = opposite / adjacent
 			--  adjacent = opposite / tanθ
 			--
-			local shadowLength = h / math.tan(-math.atan2(math.max(lightSource.z-h, .0001), e.y-lightSource.y))
+			local dz           = math.max(lightSource.z-h, .0001)
+			local shadowLength = h / math.tan(math.atan2(dz, lightSource.y-y))
 
 			--
 			--  adjacent
@@ -263,15 +292,13 @@ function love.draw()
 			--  tanθ     = opposite / adjacent
 			--  adjacent = opposite / tanθ
 			--
-			local angleL  = math.atan2(lightSource.y-e.y, (e.x-w/2)-lightSource.x)
-			local angleR  = math.atan2(lightSource.y-e.y, (e.x+w/2)-lightSource.x)
-			local offsetL = shadowLength / math.tan(angleL) / 2
-			local offsetR = shadowLength / math.tan(angleR) / 2
+			local protrusionL = shadowLength / math.tan(math.atan2(lightSource.y-y, xL-lightSource.x))
+			local protrusionR = shadowLength / math.tan(math.atan2(lightSource.y-y, xR-lightSource.x))
 
-			local x1, y1 = e.x-w/2+offsetL, e.y-shadowLength -- top left
-			local x2, y2 = e.x+w/2+offsetR, e.y-shadowLength -- top right
-			local x3, y3 = e.x+w/2, e.y -- bottom right
-			local x4, y4 = e.x-w/2, e.y -- bottom left
+			local x1, y1 = xL+protrusionL, y-shadowLength -- top left
+			local x2, y2 = xR+protrusionR, y-shadowLength -- top right
+			local x3, y3 = xR            , y              -- bottom right
+			local x4, y4 = xL            , y              -- bottom left
 
 			local u1, v1 = (qx  )/iw, (qy  )/ih -- top left
 			local u2, v2 = (qx+w)/iw, (qy  )/ih -- top right
@@ -291,39 +318,39 @@ function love.draw()
 	-- Entities.
 	for _, e in ipairs(entities) do
 		if e.type == "light" then
+			local x = e.x
+			local y = e.y - e.z
+
 			LG.setColor(1, 1, 1)
-			LG.circle("fill", e.x,e.y-e.z, 5)
+			LG.circle("fill", x,y, 5)
 
 			for i = 1, 6 do
 				local lineX = 10 * math.cos(i/6 * TAU/2)
 				local lineY = 10 * math.sin(i/6 * TAU/2)
-				LG.line(e.x+lineX,e.y-e.z+lineY, e.x-lineX,e.y-e.z-lineY)
+				LG.line(x-lineX,y-lineY, x+lineX,y+lineY)
 			end
 
 		else
-			local LIGHT_REACH       = 400
-			local TRANSITION_LENGTH = 20
+			local _,_, w,h = e.quad:getViewport()
+			local sx       = e.flip and -1 or 1
 
 			local dx       = lightSource.x - e.x
 			local dy       = lightSource.y - e.y
 			local distance = math.sqrt(dx^2 + (dy/WORLD_SCALE_Y)^2)
 
-			local lightDistance = clamp(1 - distance / LIGHT_REACH      , 0, 1) -- Darker if farther away.
-			local lightFacing   = clamp(1 + dy       / TRANSITION_LENGTH, 0, 1) -- Darker if facing away.
+			local lightDistance = clamp01(1 - distance / lightSource.reach) -- Darker if farther away.
+			local lightFacing   = clamp01(1 + dy       / LIGHT_TRANSITION ) -- Darker if facing away.
 			local light         = lightDistance * lightFacing
 
-			local scaleX   = e.flip and -1 or 1
-			local _,_, w,h = e.quad:getViewport()
-
 			LG.setColor(light, light, light)
-			LG.draw(images.sprites, e.quad, e.x,e.y, 0, scaleX,1, w/2,h, 0,0)
+			LG.draw(images.sprites, e.quad, e.x,e.y, 0, sx,1, w/2,h)
 		end
 	end
 
 	-- Info.
 	LG.origin()
 	LG.setColor(1, 1, 1)
-	LG.print("Arrow keys = move player\nMouse = move light\nPageUp/PageDown = raise/lower light", 3,1)
+	LG.print("Arrows = move player\nMouse = move light\n1/2 = raise/lower light\n3/4 = grow/shrink light", 3,1)
 end
 
 
